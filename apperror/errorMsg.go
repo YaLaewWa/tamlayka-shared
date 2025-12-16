e97d558c1477324bfc4268126d238e46e49e276d
Chantouch Narasetaphan <inecrft4747@gmail.com>
2025-12-16 10:33:40 +0700

    feat: add missing user id error message

--- apperror/errorMsg.go
+++ apperror/errorMsg.go
@@ -7,6 +7,7 @@ var INVALID_INPUT = "invalid request body"
 var AUTHENTICATE_FAILED = "username or password is incorrect"
 var MARSHAL_FAILED = "failed to marshal object to JSON"
 var SEND_MSG_FAILED = "failed to send message to queue"
+var MISSING_USER_ID = "user id not found in context"
 
 func ServiceError(serviceName string) string {
 	return strings.ToLower(serviceName) + " service failed"
